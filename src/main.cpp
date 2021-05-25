d19238956ffed0598e17b0ce57a7e70610c0f6d1
Grzegorz Koperwas <grzes869@gmail.com>
2021-05-25 15:53:32 +0200

    Added a crash popup

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
+#include <SFML/Graphics/Shader.hpp>
+#include <exception>
 #include <iostream>
+#include <string>
+#include <boxer/boxer.h>
 #include "gameloop.hpp"
 
 int main() {
-	gameloop();
+    if (!sf::Shader::isAvailable()) {
+        boxer::show(
+                "Shaders are not available on this platform, please use a gpu from later than 2005",
+                "A very sad crash handler",
+                boxer::Style::Error);
+        return 1;
+    }
+    try {
+        gameloop();
+    } catch(const std::exception& e) {
+        boxer::show(
+                (std::string("Unfortunatly we had encountered an Exception:\n\n ") + std::string(e.what())).c_str(),
+                "A very sad crash handler",
+                boxer::Style::Error);
+        return 1;
+    }
 }
